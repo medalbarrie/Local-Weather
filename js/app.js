113229ec67e20797a926fbf9bfddca216bf3e72f
medalbarrie <barriemedal@yahoo.com>
2016-10-26 22:08:31 +0100

    wether api implimented

--- js/app.js
+++ js/app.js
@@ -32,3 +32,39 @@ function getPosition(position) {
 
 };
 
+//Weather API
+
+function getUrl(lat, long, units) {
+    var appId = '16c3de9108ed16c9179c1c51008b687e';
+    return 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat +
+    '&lon=' +
+    lon + '&units=' + units + '&appid=' + appid;
+}
+
+function Unit() {
+    $('.f').is(':checked') ? units = 'imperial' : units = 'metric';
+    CallWeatherAPI();
+}
+
+function getWeather(data) {
+    var temperature = data.main.temperature
+    var temperatureUnit = units === 'metric' ? 'C' : 'F';
+    var windUnit = units === 'metric' ? 'metric/sec' : 'miles/hour';
+    var description = data.weather[0].description;
+    var code = data.weather[0].icon;
+    var windSpeed = data.wind.speed;
+    var city = data.name;
+
+
+var mainHtml = '<img src="https://openweathermap.org/img/w/' + code +
+    '.png" alt="Weather Icon" class="icon">' + '<p> ' + Math.round(temp) + ' ' + tempUnit +
+    ', ' + description + '<br> Wind Speed: ' + wspeed + windUnit + '</p><p>' +
+    city + '</p>';
+
+    $('#weather').html(mainHtml);
+    var temperatureArr = prepBackground(tempUnit);
+    setBackground(temperature, temperatureArr);
+
+};
+
+
